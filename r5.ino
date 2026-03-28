4bd7d671ec0df4732bb2781d4018f77d127bcbe4
abyes1 <133422976+abyes1@users.noreply.github.com>
2026-03-28 10:24:59 -0600

    Update r5.ino

--- r5.ino
+++ r5.ino
@@ -6,33 +6,17 @@
 PromLokiTransport transport;
 PromClient client(transport);
 
-// Create a write request for 2 series.
+// Create a write request for 1 series (matching the active time series).
 WriteRequest req(1, 1024);
 
 // Check out https://prometheus.io/docs/practices/naming/ for metric naming and label conventions.
-// This library does not currently create different metric types like gauges, counters, and histograms
-// however internally Prometheus does not differentiate between these types, rather they are both
-// a naming convention and a usage convention so it's possible to create any of these yourself.
-// See the README at https://github.com/grafana/prometheus-arduino for more info.
-
 TimeSeries ts_temp(5, "temperature", "{job=\"esp32-test\",host=\"esp32\"}");
 
 // TimeSeries ts_humid(20, "humidity", "{job=\"esp32-test\",host=\"esp32\",foo=\"bar\"}");
-
 // TimeSeries ts_pressure(20, "pressure", "{job=\"esp32-test\",host=\"esp32\"}");
-
 // TimeSeries ts_avg_curr(10, "average_current_2_sec", "{job=\"esp32-test\",host=\"esp32\"}");
-
 // TimeSeries ts_co_meas(20, "carbon_monoxide", "{job=\"esp32-test\",host=\"esp32\"}");
-
 // TimeSeries ts_sound(2, "sound_level", "{job=\"esp32-test\",host=\"esp32\"}");
-
-// !!!!!
-// IF YOU WANT TO INCREASE THE TOTAL AMOUNT OF TIME SERIES, ALSO INCREASE WriteRequest()
-// !!!!!
-
-// Note, metrics with the same name and different labels are actually different series and you would need to define them separately
-//TimeSeries ts2(5, "heap_free_bytes", "job=\"esp32-test\",host=\"esp32\",foo=\"bar\"");
 
 int loopCounter = 0;
 
@@ -40,11 +24,10 @@ void setup() {
 
     Serial.begin(115200);
     Serial.println("Starting - 1");
+
     // Wait 5s for serial connection or continue without it
-    // some boards like the esp32 will run whether or not the 
-    // serial port is connected, others like the MKR boards will wait
-    // for ever if you don't break the loop.
-    uint8_t serialTimeout;
+    // Initialized to 0 to prevent garbage memory bugs
+    uint8_t serialTimeout = 0; 
     while (!Serial && serialTimeout < 50) {
         delay(100);
         serialTimeout++;
@@ -55,14 +38,17 @@ void setup() {
     Serial.println(freeMemory());
 
     // Configure and start the transport layer
-    transport.setUseTls(false);
+    transport.setUseTls(true); // MUST be true for HTTPS port 443
     // transport.setCerts(grafanaCert, strlen(grafanaCert));
     transport.setWifiSsid(WIFI_SSID);
     transport.setWifiPass(WIFI_PASSWORD);
-    transport.setDebug(Serial);  // Remove this line to disable debug logging of the client.
+    transport.setDebug(Serial);
+
     if (!transport.begin()) {
         Serial.println(transport.errmsg);
-        while (true) {};
+        Serial.println("Transport failed. Rebooting in 5 seconds...");
+        delay(5000);
+        ESP.restart(); // Prevents the board from freezing on a network drop
     }
 
     // Configure the client
@@ -71,21 +57,19 @@ void setup() {
     client.setPort(GC_PORT);
     client.setUser(GC_USER);
     client.setPass(GC_PASS);
-    client.setDebug(Serial);  // Remove this line to disable debug logging of the client.
+    client.setDebug(Serial);
+
     if (!client.begin()) {
         Serial.println(client.errmsg);
-        while (true) {};
+        Serial.println("Client failed. Rebooting in 5 seconds...");
+        delay(5000);
+        ESP.restart(); // Prevents the board from freezing
     }
 
     // Add our TimeSeries to the WriteRequest
     req.addTimeSeries(ts_temp);
-    // req.addTimeSeries(ts_humid);
-    // req.addTimeSeries(ts_pressure);
-    // req.addTimeSeries(ts_avg_curr);
-    // req.addTimeSeries(ts_co_meas);
-    // req.addTimeSeries(ts_sound);
 
-    req.setDebug(Serial);  // Remove this line to disable debug logging of the write request serialization and compression.
+    req.setDebug(Serial);  
 
     Serial.print("Free Mem After Setup: ");
     Serial.println(freeMemory());
@@ -96,7 +80,7 @@ void setup() {
 void loop() {
     int64_t time;
     time = transport.getTimeMillis();
-    
+
     // DEBUG: Check if time sync is working
     if (time == 0) {
         Serial.println("DEBUG: Time is 0! NTP sync might have failed. Grafana will reject 0 timestamps.");
@@ -142,64 +126,3 @@ void loop() {
     Serial.println("-------------------------");
     delay(20000);
 };
-// void loop() {
-//     int64_t time;
-//     time = transport.getTimeMillis();
-//     Serial.println(time);
-
-//     // Efficiency in requests can be gained by batching writes so we accumulate a few samples before sending.
-//     // This is not necessary however, especially if your writes are infrequent. It's still recommended to build batches when you can.
-
-
-//     if (!ts_temp.addSample(time, millis())) {
-//         Serial.println(ts_temp.errmsg);
-//     }
-//     // if (!ts_humid.addSample(time, freeMemory())) {
-//     //     Serial.println(ts_humid.errmsg);
-//     // }
-//     // if (!ts_pressure.addSample(time, millis())) {
-//     //     Serial.println(ts_pressure.errmsg);
-//     // }
-//     // if (!ts_avg_curr.addSample(time, millis())) {
-//     //     Serial.println(ts_avg_curr.errmsg);
-//     // }
-//     // if (!ts_co_meas.addSample(time, millis())) {
-//     //     Serial.println(ts_co_meas.errmsg);
-//     // }
-//     // // if (% 10 = 0)
-//     // if (!ts_sound.addSample(time, millis())) {
-//     //     Serial.println(ts_sound.errmsg);
-//     // }
-//     loopCounter++;
-
-//     if (loopCounter >= 4) {
-//         // Send data
-//         loopCounter = 0;
-//         PromClient::SendResult res = client.send(req);
-//         if (!res == PromClient::SendResult::SUCCESS) {
-//             Serial.println(client.errmsg);
-//             // Note: additional retries or error handling could be implemented here.
-//             // the result could also be:
-//             // PromClient::SendResult::FAILED_DONT_RETRY
-//             // PromClient::SendResult::FAILED_RETRYABLE
-//         }
-//         // Batches are not automatically reset so that additional retry logic could be implemented by the library user.
-//         // Reset batches after a succesful send.
-//         ts_temp.resetSamples();
-//         // ts_humid.resetSamples();
-//         // ts_pressure.resetSamples();
-//         // ts_avg_curr.resetSamples();
-//         // ts_co_meas.resetSamples();
-//         // ts_sound.resetSamples();
-//     }
-
-//     // Grafana Cloud defaults to ingesting and showing one sample every 15 seconds.
-//     // If you select a higher frequency here and work with Grafana Cloud, you will need
-//     // to change the datasource's scrape interval as well. 500ms intervals are known to work and you can
-//     // try go higher if you need to.
-//     // Collection and Sending could be parallelized or timed to ensure we're on a 15 seconds cadence,
-//     // not simply add 15 second to however long collection & sending took.
-//     delay(20000);
-
-
-// };
