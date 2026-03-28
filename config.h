0afa16e71ac3d69d590aa5d9612f9d570e973135
Justin Daul <62343700+JDaul@users.noreply.github.com>
2026-03-28 10:19:03 -0600

    Add WiFi and Grafana configuration to config.h
    Added configuration for WiFi and Grafana metrics.

---
+++ config.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID     "CapstoneWiFi"
+#define WIFI_PASSWORD "RuleNumber9"
+
+// For more information on where to get these values see: https://github.com/grafana/diy-iot/blob/main/README.md#sending-metrics
+#define GC_URL "https://prometheus-prod-67-prod-us-west-0.grafana.net/"
+#define GC_PATH "/api/prom/push"
+#define GC_PORT 443
+#define GC_USER "3062594"
+#define GC_PASS "glc_eyJvIjoiMTcwODQ4NyIsIm4iOiJzdGFjay0xNTY5MTgzLWhtLXdyaXRlLXI1IiwiayI6ImQxRjBCVWUzZDlLTmY1NTdDUjI5UkY3ZSIsIm0iOnsiciI6InByb2QtdXMtd2VzdC0wIn19"
